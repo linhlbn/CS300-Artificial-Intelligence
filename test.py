64a49b5328e1b823162b5e640116492174ccb945
NaLiHg <49004582+linhlbn@users.noreply.github.com>
2023-03-01 23:23:17 +0700

    update all source code

---
+++ test.py
@@ -0,0 +1,70 @@
+import util
+
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+
+
+class SingleFoodSearchProblem(SearchProblem):
+    def __init__(self, startingGameState):
+        
+       # TODO 1
+
+
+    def getStartState(self):
+        
+      # TODO 2
+
+
+    def isGoalState(self, state):
+
+      # TODO 3
+
+    def getSuccessors(self, state):
+
+      # TODO 4
+
+    def getCostOfActions(self, actions):
+
+     # TODO 5 
